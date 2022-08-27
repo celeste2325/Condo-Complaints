e94282e5ccf030811c394b1afdc8fc3ac6ea6480
celeste lopez <celeste2325@gmail.com>
2022-08-27 19:23:25 -0300

    first commit

---
+++ src/main/java/com/gestiondereclamosdeconsorcios/reclamosDeConsorcios/ReclamosDeConsorciosApplication.java
@@ -0,0 +1,13 @@
+package com.gestiondereclamosdeconsorcios.reclamosDeConsorcios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReclamosDeConsorciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReclamosDeConsorciosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestiondereclamosdeconsorcios/reclamosDeConsorcios/ReclamosDeConsorciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestiondereclamosdeconsorcios.reclamosDeConsorcios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReclamosDeConsorciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
